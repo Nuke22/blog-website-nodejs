23a6eb9ed1848e713c3ed9f514791d86c89974d3
Yurii Fedoniuk <yura.nuke22@gmail.com>
2022-07-29 16:21:55 +0300

    123 add contact us page and about us page + add main logic of route management in my app.

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const express = require("express")
 const bodyParser = require("body-parser");
 const app = express()
 const port = 3001
+let dataArray = {"Home page": "Welcome to the home page!",
+                    "some other lorem": "lorem"}
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get("/", (req, res) => {
-    res.render("index")
+
+app.get("*", (req, res) => {
+    switch (req.url) {
+        case "/":
+            res.render("index", {data: dataArray})
+            break
+        case "/about-us":
+            break
+        case "/contact-us":
+            break
+        default:
+
+    }
 })
+// res.render("index")
 app.listen(port, () => {
     console.log("listening to port: " + port)
 })
